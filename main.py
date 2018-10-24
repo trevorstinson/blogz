bbd105904ca2457b1c5781c4f425d7d0c5522135
Trevor Stinson <trevor@trevorstinson.com>
2018-10-24 18:54:48 -0500

    Fix relationship between user and blog table

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
     pw_hash = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='author')
+    blogs = db.relationship('Blog', backref='blog.owner_id')
     
 
     def __init__(self, email, password):
@@ -36,10 +36,12 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 
 
